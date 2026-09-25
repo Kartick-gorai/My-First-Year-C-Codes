ccbbf289fc63c2a2615bd227d3a3e639613e4735
Kartick Gorai <kartickgorai0001@gmail.com>
2026-09-25 16:44:22 +0530

    my first code of pattern printing

---
+++ alphabetTriangle2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    char ch = 'A';
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n - i; j++){
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++){
+            printf("%c ",ch++);
+        }
+        ch = 'A';
+        printf("\n");
+    }
+}
